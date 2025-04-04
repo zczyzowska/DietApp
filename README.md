3ac7397821ae1849c69ad7725d8826ba1637dd7f
czyzowsz <01178829@pw.edu.pl>
2025-04-04 11:32:39 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DietApp
-A mobile app for Android and iOS to manage your diet and calculate calories based on a photo of your meal.
---
+++ android/app/src/main/kotlin/com/example/diet_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.diet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
